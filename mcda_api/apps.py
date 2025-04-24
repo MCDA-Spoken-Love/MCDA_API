18f149a99433cd71383b72eb65ce62c7001a37e4
MatheusBlanco <msallesblanco@gmail.com>
2025-04-24 16:31:00 -0300

    feat: :card_file_box: Perform migrations and create models

--- mcda_api/apps.py
+++ mcda_api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ApiConfig(AppConfig):
+class McdaApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
+    name = 'mcda_api'
