7cbb7fe2a95f5881bd9fce07267a336a0ceb4299
MatheusBlanco <msallesblanco@gmail.com>
2025-05-19 21:06:35 -0300

    feat: :sparkles: Adds user update relationship

--- Account/urls.py
+++ Account/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
     path('user/search/', views.get_user_by_filter, name='get_user_by_username'),
+    path('user/relationship/', views.manager_user_relationshop,
+         name='manager_user_relationship'),
 ]
